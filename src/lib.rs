0299f6fb3523b13d1c6ff6bfddc58fecb99db2cf
Jihoon Kim <kjh618@kaist.ac.kr>
2021-04-02 16:19:56 +0900

    Implement post2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 pub mod post1;
+pub mod post2;
 
 #[cfg(test)]
 mod tests {
     use crate::post1;
+    use crate::post2;
 
     #[test]
     fn post1_test() {
@@ -36,5 +38,18 @@ mod tests {
         // PendingReview -> Published
         post.approve();
         assert_eq!("ac", post.content());
+    }
+
+    #[test]
+    fn post2_test() {
+        let mut post = post2::Post::new();
+
+        post.add_text("I ate a salad for lunch today");
+
+        let post = post.request_review();
+
+        let post = post.approve();
+
+        assert_eq!("I ate a salad for lunch today", post.content());
     }
 }
