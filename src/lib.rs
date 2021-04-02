d3b10f1df552411cf7bab760147cdb6c332814d3
Jihoon Kim <kjh618@kaist.ac.kr>
2021-04-02 16:21:14 +0900

    Add reject

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,10 @@ mod tests {
 
         let post = post.request_review();
 
+        let post = post.reject();
+
+        let post = post.request_review();
+
         let post = post.approve();
 
         assert_eq!("I ate a salad for lunch today", post.content());
